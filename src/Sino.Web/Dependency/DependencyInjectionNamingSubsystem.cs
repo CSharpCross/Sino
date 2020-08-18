01672816e466fabcb2555abb84980ccf0d512ac2
姚正发 <976691141@qq.com>
2020-08-18 17:16:08 +0800

    Add DependencyInjectionNamingSubsystem

--- src/Sino.Web/Dependency/DependencyInjectionNamingSubsystem.cs
+++ src/Sino.Web/Dependency/DependencyInjectionNamingSubsystem.cs
@@ -4,6 +4,8 @@ using Castle.MicroKernel.Util;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
+using System.Reflection;
 using System.Text;
 
 namespace Sino.Web.Dependency
@@ -26,7 +28,61 @@ namespace Sino.Web.Dependency
         {
             _ = service ?? throw new ArgumentNullException();
 
+            if (filters != null)
+            {
+                var filtersOpinion = GetFiltersOpinion(service);
+                if (filtersOpinion != null)
+                {
+                    return filtersOpinion;
+                }
+            }
 
+            IHandler[] result;
+            using (var locker = @lock.ForReadingUpgradeable())
+            {
+                if (_handlerListsRegistrationOrderByTypeCache.TryGetValue(service, out result))
+                {
+                    return result;
+                }
+                result = GetHandlersInRegisterOrderNoLock(service);
+
+                locker.Upgrade();
+                _handlerListsRegistrationOrderByTypeCache[service] = result;
+            }
+
+            return result;
+        }
+
+        public override IHandler GetHandler(Type service)
+        {
+            _ = service ?? throw new ArgumentNullException(nameof(service));
+
+            if (selectors != null)
+            {
+                var selectorsOpinion = GetSelectorsOpinion(null, service);
+                if (selectorsOpinion != null)
+                {
+                    return selectorsOpinion;
+                }
+            }
+            if (HandlerByServiceCache.TryGetValue(service, out var handler))
+            {
+                return handler;
+            }
+            
+            if (service.GetTypeInfo().IsGenericType && service.GetTypeInfo().IsGenericTypeDefinition == false)
+            {
+                var openService = service.GetGenericTypeDefinition();
+                if (HandlerByServiceCache.TryGetValue(openService, out handler) && handler.Supports(service))
+                {
+                    return handler;
+                }
+
+                var handlerCandidates = base.GetHandlers(openService);
+                return handlerCandidates.First(x => x.Supports(service));
+            }
+
+            return null;
         }
     }
 }
