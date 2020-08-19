d58fefedf1f0f7048b24f222cd81359862aba389
姚正发 <976691141@qq.com>
2020-08-19 15:01:10 +0800

    暂存

--- src/Sino.Web/Dependency/SinoScopedServiceProvider.cs
+++ src/Sino.Web/Dependency/SinoScopedServiceProvider.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Castle.Windsor;
+using Microsoft.Extensions.DependencyInjection;
+using Sino.Web.Dependency.Scope;
 using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Reflection;
 
 namespace Sino.Web.Dependency
 {
@@ -10,6 +12,70 @@ namespace Sino.Web.Dependency
     /// </summary>
     internal class SinoScopedServiceProvider : IServiceProvider, ISupportRequiredService, IDisposable
     {
-        
+        private readonly ExtensionContainerScope _scope;
+
+        private bool _disposing = false;
+
+        private readonly IWindsorContainer _container;
+
+        public SinoScopedServiceProvider(IWindsorContainer container)
+        {
+            _container = container;
+            _scope = ExtensionContainerScope.Current;
+        }
+
+        public object GetService(Type serviceType)
+        {
+            using(var fs = new ExtensionContainerScope.ForcedScope(_scope))
+            {
+                return ResolveInstanceOrNull(serviceType, true);
+            }
+        }
+
+        public object GetRequiredService(Type serviceType)
+        {
+            using(var fs = new ExtensionContainerScope.ForcedScope(_scope))
+            {
+                return ResolveInstanceOrNull(serviceType, false);
+            }
+        }
+
+        public void Dispose()
+        {
+            if (_scope is ExtensionContainerScope)
+            {
+                if (!_disposing)
+                {
+                    _disposing = true;
+                    var disposableScope = _scope as IDisposable;
+                    if (disposableScope != null)
+                    {
+                        disposableScope.Dispose();
+                    }
+                    _container.Dispose();
+                }
+            }
+        }
+
+        private object ResolveInstanceOrNull(Type serviceType, bool isOptional)
+        {
+            if (_container.Kernel.HasComponent(serviceType))
+            {
+                return _container.Resolve(serviceType);
+            }
+
+            if (serviceType.GetTypeInfo().IsGenericType && serviceType.GetGenericTypeDefinition() == typeof(IEnumerable<>))
+            {
+                var allObjects = _container.ResolveAll(serviceType.GenericTypeArguments[0]);
+                return allObjects;
+            }
+
+            if (isOptional)
+            {
+                return null;
+            }
+
+            return _container.Resolve(serviceType);
+        }
     }
 }
