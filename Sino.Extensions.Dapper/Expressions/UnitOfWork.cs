709f0aa31e878124d62517d230e685eca5c53e85
刘飞 <251168923@qq.com>
2020-08-22 14:36:11 +0800

    迁移Dapper 并且重构Aop事务实现方案

---
+++ Sino.Extensions.Dapper/Expressions/UnitOfWork.cs
@@ -0,0 +1,76 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Sino.Extensions.Dapper.Expressions
+{
+    /// <summary>
+    /// 工作单元
+    /// </summary>
+    public class UnitOfWork : IUnitOfWork
+    {
+        private bool disposed;
+        private IDbTransaction trans = null;
+
+        /// <summary>
+        /// 事务
+        /// </summary>
+        public IDbTransaction DbTransaction { get { return trans; } }
+
+        /// <summary>
+        /// 数据连接
+        /// </summary>
+        public IDbConnection WriteConnection { get; set; }
+
+        protected IDapperConfiguration Configurationn { get; set; }
+
+        public UnitOfWork(IDapperConfiguration configuration)
+        {
+            Configurationn = configuration;
+            WriteConnection = new MySqlConnection(Configurationn.WriteConnectionString);
+            WriteConnection.Open();
+        }
+
+        /// <summary>
+        /// 开启事务
+        /// </summary>
+        public void BeginTransaction()
+        {
+            trans = WriteConnection.BeginTransaction();
+        }
+
+        /// <summary>
+        /// 完成事务
+        /// </summary>
+        public void Commit() => trans?.Commit();
+
+        /// <summary>
+        /// 回滚事务
+        /// </summary>
+        public void Rollback() => trans?.Rollback();
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~UnitOfWork() => Dispose(false);
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+                return;
+            if (disposing)
+            {
+                trans?.Dispose();
+                WriteConnection?.Dispose();
+            }
+            trans = null;
+            WriteConnection = null;
+            disposed = true;
+        }
+    }
+}
---
+++ Sino.Extensions.Dapper/IDapperConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Sino.Dependency;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sino.Extensions.Dapper
+{
+    public interface IDapperConfiguration : ISingletonDependency
+    {
+        /// <summary>
+        /// 写连接字符串
+        /// </summary>
+		string WriteConnectionString { get; set; }
+
+        /// <summary>
+        /// 读连接字符串
+        /// </summary>
+        string ReadConnectionString { get; set; }
+    }
+}
