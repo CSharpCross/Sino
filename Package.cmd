9887c568adbfc3526822682b722f1d66f047c92e
姚正发 <976691141@qq.com>
2020-11-20 22:54:51 +0800

    修改发版脚本

--- Package.cmd
+++ Package.cmd
@@ -9,33 +9,39 @@ echo 3. OrvilleX.Dapper
 echo 4. OrvilleX.AutoIndex
 echo 5. OrvilleX.Cache
 echo 6. OrvilleX.MongoDB
+echo 7. OrvilleX.AutoMapper
 
+apikey = "";
 set /p packageNum=请输入编号：
 
 if %packageNum% == 1 (
 	echo 开始对OrvilleX发版
 	dotnet pack .\src\OrvilleX -c Release -o .\artifacts
-	dotnet nuget push .\artifacts\*.nupkg -k [密钥] -s https://api.nuget.org/v3/index.json
+	dotnet nuget push .\artifacts\*.nupkg -k %apikey% -s https://api.nuget.org/v3/index.json
 ) else if %packageNum% == 2 (
 	echo 开始对OrvilleX.EventBus发版
 	dotnet pack .\src\OrvilleX.EventBus -c Release -o .\artifacts
-	dotnet nuget push .\artifacts\*.nupkg -k [密钥] -s https://api.nuget.org/v3/index.json   
+	dotnet nuget push .\artifacts\*.nupkg -k %apikey% -s https://api.nuget.org/v3/index.json   
 ) else if %packageNum% == 3 (
 	echo 开始对OrvilleX.Dapper发版
 	dotnet pack .\src\OrvilleX.Dapper -c Release -o .\artifacts
-	dotnet nuget push .\artifacts\*.nupkg -k [密钥] -s https://api.nuget.org/v3/index.json  
+	dotnet nuget push .\artifacts\*.nupkg -k %apikey% -s https://api.nuget.org/v3/index.json  
 ) else if %packageNum% == 4 (
     echo 开始对OrvilleX.AutoIndex发版
     dotnet pack .\src\OrvilleX.AutoIndex -c Release -o .\artifacts
-    dotnet nuget push .\artifacts\*.nupkg -k [密钥] -s https://api.nuget.org/v3/index.json
+    dotnet nuget push .\artifacts\*.nupkg -k %apikey% -s https://api.nuget.org/v3/index.json
 ) else if %packageNum% == 5 (
 	echo 开始对OrvilleX.Cache发版
 	dotnet pack .\src\OrvilleX.Cache -c Release -o .\artifacts
-	dotnet nuget push .\artifacts\*.nupkg -k [密钥] -s https://api.nuget.org/v3/index.json
+	dotnet nuget push .\artifacts\*.nupkg -k %apikey% -s https://api.nuget.org/v3/index.json
 ) else if %packageNum% == 6 (
-	echo 开发对OrvilleX.MongoDB发版
+	echo 开始对OrvilleX.MongoDB发版
 	dotnet pack .\src\OrvilleX.MongoDB -c Release -o .\artifacts
-	dotnet nuget push .\artifacts\*.nupkg -k [密钥] -s https://api.nuget.org/v3/index.json
+	dotnet nuget push .\artifacts\*.nupkg -k %apikey% -s https://api.nuget.org/v3/index.json
+) else if %packageNum% == 7 (
+	echo 开始对OrvilleX.AutoMapper发版
+	dotnet pack .\src\OrvilleX.AutoMapper -c Release -o .\artifacts
+	dotnet nuget push .\artifacts\*.nupkg -k %apikey% -s https://api.nuget.org/v3/index.json
 )
 rd /s/q .\artifacts
 
