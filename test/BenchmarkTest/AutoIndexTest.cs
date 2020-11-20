4d2b72421b564502c882506f117b056f04c000de
姚正发 <976691141@qq.com>
2020-11-20 21:59:11 +0800

    增加基准测试项目

---
+++ test/BenchmarkTest/AutoIndexTest.cs
@@ -0,0 +1,38 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+using BenchmarkDotNet.Jobs;
+using Microsoft.Extensions.DependencyInjection;
+using OrvilleX.AutoIndex;
+using System.Threading.Tasks;
+
+namespace BenchmarkTest
+{
+    [SimpleJob(RunStrategy.Throughput, RuntimeMoniker.NetCoreApp20)]
+    [MinColumn, MaxColumn, MeanColumn, MedianColumn]
+    public class AutoIndexTest
+    {
+        private ITinyIdClient _client;
+
+        public AutoIndexTest()
+        {
+            ServiceCollection services = new ServiceCollection();
+            services.AddLogging();
+            services.AddHttpClient();
+            services.AddTinyId("0f673adf80504e2eaa552f5d791b644c", "localhost");
+
+            _client = services.BuildServiceProvider().GetService<ITinyIdClient>();
+        }
+
+        [Benchmark]
+        public async Task SingleNextId()
+        {
+            _ = await _client.NextId("test");
+        }
+
+        [Benchmark]
+        public async Task BatchNextId()
+        {
+            _ = await _client.NextId("test", 1000);
+        }
+    }
+}
---
+++ test/BenchmarkTest/RedisTest.cs
@@ -0,0 +1,32 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+using BenchmarkDotNet.Jobs;
+using StackExchange.Redis;
+using System;
+using System.Threading.Tasks;
+
+namespace BenchmarkTest
+{
+    [SimpleJob(RunStrategy.Throughput, RuntimeMoniker.NetCoreApp20)]
+    [MinColumn, MaxColumn, MeanColumn, MedianColumn]
+    public class RedisTest
+    {
+        private IDatabase _database;
+
+        public RedisTest()
+        {
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("192.168.3.40:6379");
+            _database = redis.GetDatabase();
+        }
+
+        [Benchmark]
+        public async Task StringGetAndSetTest()
+        {
+            Random r = new Random();
+            var key = r.Next(1000);
+            await _database.StringSetAsync(key.ToString(), "testfefefaf");
+
+            string value = await _database.StringGetAsync(key.ToString());
+        }
+    }
+}
